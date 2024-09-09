b5ad2874c8ef22503857301da4a288174690b534
Tauno Tamm. <tauno171@gmail.com>
2024-09-09 14:42:00 +0300

    create new app

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿//This is a comment on my code
+
 string nimi;
 
 Console.WriteLine("Enter your name:");
@@ -6,4 +7,4 @@ nimi = Console.ReadLine();
 
 Console.WriteLine("Hello" + ", " + nimi + "!");
 
-Console.WriteLine($"Hello, {nimi} !");
+Console.WriteLine($"Hello, {nimi}!");
